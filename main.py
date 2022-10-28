8f51930ff78047b6e28577da95b02127ff922b3e
drkus-dev <drkus@kakao.com>
2022-10-28 12:18:33 +0900

    selenium4 업그레이드
    주로 삽질
    1. 자바 깔지 않고 패키징 시도
    2. selenium3와 문법 틀림 (webdriver import 부분과 webdriver 기존 수동 설치에서 자동으로 바뀜 근데 빡치는게 이부분 구글링 및 네이버 블로그 해야함)
    3. github.io 웹이나 기존 naver 웹사이트 get 함수로 열리는데 1초 열림

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-a = 20
-b = 30
-c = a+b
-print(a,b,c)
+
+# selenium4
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+# 이 부분.. 자바를 설치 안 하면 패키징 관련 에러 무지 뜸
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+#driver 객체 만들고 url 문자열을 driver.get 함수로 호출
+#DRIVER_PATH = 'Users/kys/Documents/GitHub/selenium/chromedriver.exe'
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches",["enable-loggin"])
+
+service_obj = Service(r"/Users/kys/Documents/GitHub/selenium/chromedriver.exe")
+
+DRIVER_PATH = ChromeDriverManager().install()
+print(DRIVER_PATH,'<<경로')
+#driver = webdriver.Chrome(options=options, service=service_obj)
+driver = webdriver.Chrome(DRIVER_PATH)
+
+#n초 딜레이
+driver.implicitly_wait(4)
+driver.get('https://www.naver.com')
