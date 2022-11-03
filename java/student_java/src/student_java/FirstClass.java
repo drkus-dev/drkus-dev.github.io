3fe4e2ff47777eec14158dfdbb87037fd9e25368
drkus-dev <drkus@kakao.com>
2022-11-04 08:45:13 +0900

    java 문법 공부
    조건문 반복문 속 continue는 break와 다르게 for문을 나가지 않고 다시 for 로직을 진행한다.

--- java/student_java/src/student_java/FirstClass.java
+++ java/student_java/src/student_java/FirstClass.java
@@ -5,6 +5,19 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello java.2.");
+		
+	int i, j, k;
+		for(i = 1, j = 1, k = 0; i < 5; i++ ) {
+			if((i%2) == 0) {
+				System.out.println(i);
+				System.out.println(j);
+				System.out.println(k);
+				System.out.println("정수 end");
+				continue;
+				
+			}
+			k += i * j++;
+		}
+		System.out.println(k);
 	}
-
 }
